Attempt to fix NaN issues with KL minimisation

experiments/kldivergence.py
@@ -2,6 +2,14 @@ from math import pi
 import tensorflow as tf
 
 
+def safe_log(x, eps=1e-5):
+    """
+    tf.Node -> Float? -> tf.Node
+    Return the natural logarithm of a value with an added epsilon.
+    """
+    return tf.log(x + eps)
+
+
 def kl_estimator(sample_p, sample_q):
     """
     tf.Node -> tf.Node -> tf.Node
@@ -9,7 +17,7 @@ def kl_estimator(sample_p, sample_q):
     to q, given the probability densities of a number of samples under
     p and q respectively.
     """
-    return tf.reduce_mean(tf.log(sample_p / sample_q))
+    return tf.reduce_mean(safe_log(sample_p / sample_q))
 
 
 def gaussian_sampler(mean, stddev, batch_size):
@@ -41,7 +49,7 @@ def gaussian_divergence(mu_a, sigma_a, mu_b, sigma_b):
     Return a node that calculates the KL divergence of two Gaussian
     probability distributions.
     """
-    log_term = tf.log(sigma_b / sigma_a)
+    log_term = safe_log(sigma_b / sigma_a)
     numerator = tf.square(sigma_a) + tf.square(mu_a - mu_b)
     denominator = 2 * tf.square(sigma_b)
     return log_term + (numerator / denominator) - 0.5
@@ -53,4 +61,27 @@ def run():
     Run an experiment, trying to determine whether KL divergence
     can be accurately estimated using an average of samples.
     """
-    pass
+    batch_size = 2048
+
+    mu_a = tf.Variable(0.)
+    sigma_a = tf.Variable(1.)
+    mu_b = tf.constant(0.5)
+    sigma_b = tf.constant(0.5)
+    x = gaussian_sampler(mu_a, sigma_a, batch_size)
+    p = gaussian_pdf(mu_a, sigma_a)(x)
+    q = gaussian_pdf(mu_b, sigma_b)(x)
+
+    kl = kl_estimator(p, q)
+    kl_real = gaussian_divergence(mu_a, sigma_a, mu_b, sigma_b)
+
+    optimised_node = kl if True else tf.log(kl)
+    minimiser = tf.train.AdamOptimizer().minimize(optimised_node)
+
+    s = tf.Session()
+    s.run(tf.global_variables_initializer())
+
+    for i in range(10000):
+        output = s.run([mu_a, sigma_a, kl, kl_real, minimiser])
+        if i % 150 == 0:
+            print('mu: {}, sigma: {}, kl: {}, kl_real: {}'.format(
+                output[0], output[1], output[2], output[3]))