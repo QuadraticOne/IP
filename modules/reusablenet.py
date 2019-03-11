63201270579b37a9990219587a761a453413a5bf
aatack <alexanderjtatack@gmail.com>
2019-03-11 20:52:50 +0000

    Make reusable implementation of feedforward layer

--- modules/reusablenet.py
+++ modules/reusablenet.py
@@ -1,3 +1,6 @@
+import tensorflow as tf
+
+
 def deep_copy(value):
     """Create a deep copy of a JSON-like object."""
     if isinstance(value, dict):
@@ -6,3 +9,25 @@ def deep_copy(value):
         return [deep_copy(v) for v in value]
     else:
         return value
+
+
+def feedforward_layer(input_dict):
+    """
+    Create a feedforward layer from an input dictionary.
+    
+    The input dictionary should contain entries, in the form of
+    tensorflow nodes, for the input, weights, and biases.  A new
+    entry will be added for the output node.  The activation should
+    be specified as a string; one of 'leaky-relu', 'relu', 'sigmoid',
+    or 'tanh'.
+    """
+    activation = {
+        'sigmoid': tf.nn.sigmoid,
+        'relu': tf.nn.relu,
+        'leaky-relu': tf.nn.leaky_relu,
+        'tanh': tf.nn.tanh
+    }[input_dict['activation']]
+    copy = deep_copy(input_dict)
+    copy['output'] = activation(tf.matmul(
+        input_dict['weights'], input_dict['input']) + input_dict['biases'])
+    return copy
