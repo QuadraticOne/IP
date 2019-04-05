8f88a978a59e8b74f1a8f565ef586745ccb81a80
aatack <alexanderjtatack@gmail.com>
2019-04-05 23:37:27 +0100

    Add function for retrieving group statistics on a logged constraint

--- production/plots/solutions.py
+++ production/plots/solutions.py
@@ -9,3 +9,34 @@ def get_solution_statistics(solution):
         solution["relativeDensity"],
         solution["type"],
     )
+
+
+def get_constraint_statistics(constraint):
+    """
+    Dict -> ([Float], [Float])
+    Return two lists, the first containing the satisfaction probabilities of all
+    generated solutions to a logged constraint, and the second containing the
+    relative densities of all true solutions to that constraint.
+    """
+    generated_solutions, true_solutions = partition(
+        lambda s: s["type"] == "generated", constraint["solutions"]
+    )
+    return (
+        [s["satisfactionProbability"] for s in generated_solutions],
+        [s["relativeDensity"] for s in true_solutions],
+    )
+
+
+def partition(predicate, values):
+    """
+    (a -> Bool) -> [a] -> ([a], [a])
+    Partition a list of values into two lists, the first containing those values
+    that satisfy the predicate, and the second those that do not.
+    """
+    trues, falses = [], []
+    for value in values:
+        if predicate(value):
+            trues.append(value)
+        else:
+            falses.append(value)
+    return trues, falses
