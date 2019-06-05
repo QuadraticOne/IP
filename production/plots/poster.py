1693487b053ae5c04b5ebe266a952f35915d32df
aatack <alexanderjtatack@gmail.com>
2019-06-05 17:39:56 +0100

    Include axis labels with Branin function plot

--- production/plots/poster.py
+++ production/plots/poster.py
@@ -21,12 +21,16 @@ def plot_branin_function(n=200, lower=0.0, upper=1.0):
     xs, ys = np.meshgrid(xs, ys)
     vectorised = np.vectorize(branin_function)
     zs = vectorised(xs, ys)
-    print(zs.shape)
 
     surface = axes.plot_surface(
         xs, ys, zs, cmap=cm.viridis, linewidth=0, antialiased=False
     )
+
+    axes.set_xlabel("$s_1$")
+    axes.set_ylabel("$s_2$")
+    axes.set_zlabel("$h(s_1, s_2)$")
     axes.set_xlim(0, 1)
     axes.set_ylim(0, 1)
     axes.set_zlim(0, 1)
+
     plt.show()
